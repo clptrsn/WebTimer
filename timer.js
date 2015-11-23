96fe8713e436e959b958aacff83ba9825a33e939
Cole Peterson <cspeter1998@gmail.com>
2015-11-22 23:45:01 -0600

    Added log output of times
    The html skeleton was modified to better accomadate logging
    the time.
    
    This commit also improves the formatting of the timer by
    dynamically removing the minutes and hours if they are zero
    value.

--- timer.js
+++ timer.js
@@ -26,6 +26,7 @@ var timer = function() {
 		return this.elapsed;		
 	};
 
+
 	this.stopTimer = function() {
 		this.stop = Date.now();
 		this.elapsed = this.stop - this.startTime;
@@ -53,7 +54,8 @@ var timer = function() {
 
 		hundred = Math.floor(hundred / 10);
 
-		timeValues = [hours, minutes, seconds, hundred];
+		if(hours == 0) timeValues = [minutes, seconds, hundred];
+		else timeValues = [hours, minutes, seconds, hundred];
 
 		for ( var i = 0; i < timeValues.length; i++) {
 			if( i == 0 && timeValues[i] == 0) continue;
@@ -61,6 +63,7 @@ var timer = function() {
 			var seperator;
 			if( i < timeValues.length - 2) seperator = ':'; 
 			else seperator = '.';
+
 
 			result += padding(timeValues[i], 2) + seperator;
 		};
@@ -72,7 +75,7 @@ var timer = function() {
 function padding( numberIn, padding) {
 	var numberString = numberIn.toString();
 	numberString = numberString.split('').reverse().join('');
-	var zeros =  padding - numberString.length ;
+	var zeros =  padding - numberString.length;
 
 	for( var i = 0; i < zeros; i++) {
 		numberString += '0';
@@ -81,12 +84,37 @@ function padding( numberIn, padding) {
 	return numberString.split('').reverse().join('');
 }
 
+function logTimes( timeAr ) {
+	var prev = $("#times").children("p").html();
+	var seperator = "";
+	alert(prev)
+	if( prev != "") seperator = ", ";
+
+	var id = timeAr.length - 1;
+	var time = getTimes(timeAr[id]);
+	$("#times").children("p").append(seperator + "<span id = \"solve" + id + "\">" + time + "</span>"); 
+}
+
+function getTimes( time ) {
+	time += '';
+	var timeComponents = time.split(":");
+	if( timeComponents.length == 1) {
+
+		return timeComponents[0];
+
+	} else {
+
+		return Number(timeComponents[0]) + ":" + timeComponents[1];
+	}
+}
+
 
 $(document).ready( function() {
 
 	var cubeTimer = new timer();
 	var timerText = '';
 	var timerUpdate;
+	var times = [];
 
 	function updateTimer() {
 		timerText = cubeTimer.formatTime();
@@ -94,6 +122,7 @@ $(document).ready( function() {
 	}
 
 	updateTimer();
+
 	$(document.body).on( "keyup", function( e ) {
 		if( e.keyCode == 32) {
 			if( !cubeTimer.isRunning()) {
@@ -103,7 +132,12 @@ $(document).ready( function() {
 				cubeTimer.stopTimer();
 				updateTimer();
 				clearInterval(timerUpdate);
+				times.push(cubeTimer.formatTime());
+alert(times);
+				logTimes(times);
+
 				cubeTimer.resetTimer();
+
 			}
 		}
 	});

8183f7787c521b0a56bdbca9c1d989fa0168fb90
Cole Peterson <cspeter1998@gmail.com>
2015-11-24 01:36:41 -0600

    Toggable Timer Options
    The show timer was given functionality allowing the user to
    access the options and allowing the main layout to remain less
    cluttered.

--- timerOptions.js
+++ timerOptions.js
@@ -1,14 +1,35 @@
 $(document).ready( function() {
+	$("#optionShow").on("click", function() {
+		if($("#option").is(':visible')) {
+			$("#option").hide();
+			changeHeightDivs(0);
+		} else {
+			$("#option").show();
+			changeHeightDivs(-50);
+		}
+	});
+
+	function changeHeightDivs( change ) {
+		$("#timer").height("calc(100% - 78px + " + change + "px)");
+		$("#stats").height("calc(100% - 78px + " + change + "px)");
+		$("#times").height("calc(100% - 78px + " + change + "px)");
+	}
+
 	$("#larger").on("click", function () {
 		var currentSize = parseInt($("#timerOut").css("font-size"));
 		var newSize = currentSize + 10;
-		alert(currentSize);
-		$("timerOut").css("font-size", newSize);
+
+		var optionHeight = $("#timer").height() / 2 + newSize;
+		$("#timerOut").css("fontSize", newSize);
+		$("#optionShow").css({"top": optionHeight });
 	});
 
 	$("#smaller").on("click", function () {
 		var currentSize = parseInt($("#timerOut").css("font-size"));
 		var newSize = currentSize - 10;
-		$("timerOut").css("font-size", newSize);
+
+		var optionHeight = $("#timer").height() / 2 + newSize;
+		$("#timerOut").css("fontSize", newSize);
+		$("#optionShow").css({"top": optionHeight });
 	});
 });
